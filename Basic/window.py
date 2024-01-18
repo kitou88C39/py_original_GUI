5f85b012656f8864ac9569405940977e9259d607
kitou88C39 <kanfutrooper@icloud.com>
2024-01-18 20:38:14 +0900

    Windowの作成

--- Basic/window.py
+++ Basic/window.py
@@ -5,6 +5,7 @@ root = tkinter.Tk()
 root.title('Window practice')
 root.iconbitmap('icon.ico')
 root.geometry('300×800')
-
+root.resizable(0, 0) #変更禁止
+root.config(bg='red') #背景色
 # ウィンドウのループ処理
 root.mainloop()

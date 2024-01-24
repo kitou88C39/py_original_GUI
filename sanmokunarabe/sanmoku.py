f2cfc00a4af413fc212fe2bd1ef16b66502009c1
kitou88C39 <kanfutrooper@icloud.com>
2024-01-24 16:16:59 +0900

    Create a crawler using scrapy

--- sanmokunarabe/sanmoku.py
+++ sanmokunarabe/sanmoku.py
@@ -1,10 +1,12 @@
 import tkinter
 
 def masume():
-   cvs.create_line(200,0,200,600,fill="gray", width=8) # 左の縦線
-   cvs.create_line(400,0,400,600,fill="gray", width=8) # 右の縦線
-   cvs.create_line(0,200,600,200,fill="gray", width=8) # 上の横線
-   cvs.create_line(0,400,600,400,fill="gray", width=8) # 下の横線
+#    cvs.create_line(200,0,200,600,fill="gray", width=8) # 左の縦線
+#    cvs.create_line(400,0,400,600,fill="gray", width=8) # 右の縦線
+#    cvs.create_line(0,200,600,200,fill="gray", width=8) # 上の横線
+#    cvs.create_line(0,400,600,400,fill="gray", width=8) # 下の横線
+
+    for i in range(1,3)
 
 root=tkinter.Tk()
 root.title("三目並べ")

fc3a44871517da688a5f21532a7d4165599e7ae1
kitou88C39 <kanfutrooper@icloud.com>
2024-01-24 16:00:48 +0900

    マスを作成する

--- sanmokunarabe/sanmoku.py
+++ sanmokunarabe/sanmoku.py
@@ -5,3 +5,6 @@ def masume():
    cvs.create_line(400,0,400,600,fill="gray", width=8) # 右の縦線
    cvs.create_line(0,200,600,200,fill="gray", width=8) # 上の横線
    cvs.create_line(0,400,600,400,fill="gray", width=8) # 下の横線
+
+root=tkinter.Tk()
+root.title("三目並べ")
